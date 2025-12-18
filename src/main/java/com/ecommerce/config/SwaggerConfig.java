a0792fb5cde85f9434e99a78fa953cb77c785e4c
AnishShrestha <anishnitro5@gmail.com>
2025-12-18 21:58:07 +0545

    configured swagger

---
+++ src/main/java/com/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.ecommerce.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI myCustomConfig(){
+        return new OpenAPI().info(
+                new Info().title("The CutLab APIs")
+                        .description("Ecommerce Project with product and slot recommendation along with path optimized path for delivery")
+
+        ).servers(List.of(
+                new Server().url("http://localhost:8080").description("local"),
+                new Server().url("http://localhost:8082").description("live")
+        ));
+    }
+
+}
