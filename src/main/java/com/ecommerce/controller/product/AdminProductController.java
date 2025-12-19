cf317f23eb32dc81e60f6c6a0868708201bf9fe8
AnishShrestha <anishnitro5@gmail.com>
2025-12-20 00:25:18 +0545

    admin side management of products

---
+++ src/main/java/com/ecommerce/controller/product/AdminProductController.java
@@ -0,0 +1,167 @@
+package com.ecommerce.controller.product;
+
+
+import com.ecommerce.dto.request.product.BrandRequest;
+import com.ecommerce.dto.request.product.CategoryRequest;
+import com.ecommerce.dto.request.product.ProductRequest;
+import com.ecommerce.dto.request.product.TagRequest;
+import com.ecommerce.dto.response.ApiResponse;
+import com.ecommerce.dto.response.product.SingleProductWithCostPriceResponse;
+import com.ecommerce.service.product.AdminProductService;
+import com.ecommerce.validation.ValidId;
+import com.ecommerce.validation.ValidPrice;
+import com.ecommerce.validation.ValidQuantity;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/admin/products")
+public class AdminProductController {
+
+    private final AdminProductService adminProductService;
+
+//    --------main page maa huney operations haru-----------
+//    for adding brand : format form maa xa mero desktop ko
+    @PostMapping(value = "/brand", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<?>> addBrand(
+            @Valid @RequestPart("addBrandRequest") BrandRequest brandRequest,
+            @RequestPart("logo") MultipartFile logo
+    ){
+        adminProductService.addBrand(brandRequest, logo);
+        return ResponseEntity.ok(ApiResponse.ok("Brand added successfully"));
+    }
+
+//    for adding category
+    @PostMapping(value = "/category", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<?>> addCategory(
+            @Valid @RequestPart("addCategoryRequest")CategoryRequest addCategoryRequest,
+            @RequestPart("image") MultipartFile image
+    ){
+        adminProductService.addCategory(addCategoryRequest, image);
+        return ResponseEntity.ok(ApiResponse.ok("Category added successfully"));
+    }
+
+//    for adding tags
+    @PostMapping("/tags")
+    public ResponseEntity<ApiResponse<String>> addTags(
+            @Valid @RequestBody TagRequest tagRequests
+    ){
+        adminProductService.addTags(tagRequests);
+        return ResponseEntity.ok(
+                ApiResponse.ok("Tags added successfully"));
+    }
+
+    // send brand and category slug ---for adding product
+    @PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<SingleProductWithCostPriceResponse>> addNewProduct(
+            @Valid @RequestPart("addProductRequest") ProductRequest addProductRequest,
+            @RequestPart("imageFiles") List<MultipartFile> imageFiles
+    ){
+        SingleProductWithCostPriceResponse response = adminProductService.addNewProduct(addProductRequest, imageFiles);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.ok(response, "New product added successfully"));
+    }
+
+
+
+
+//    --------specific product detail page maa huney operations haru-----------
+//    for getting detail of specific product
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<SingleProductWithCostPriceResponse>> getAdminDetailOfProduct(
+            @ValidId @PathVariable Long id
+    ){
+        SingleProductWithCostPriceResponse product = adminProductService.getAdminDetailOfProduct(id);
+        return ResponseEntity.ok(ApiResponse.ok(product, "Fetched successfully"));
+    }
+
+//    for updating price of product
+    @PutMapping("/{id}/price")
+    public ResponseEntity<ApiResponse<?>> updateProductPrice(
+            @ValidId @PathVariable Long id,
+            @ValidPrice @RequestParam BigDecimal price
+    ){
+        adminProductService.updatePrice(id, price);
+        return ResponseEntity.ok(ApiResponse.ok("Price updated successfully"));
+    }
+
+//    for updating quantity/stock of product
+    @PutMapping("/{id}/quantity")
+    public ResponseEntity<ApiResponse<?>> updateProductQuantity(
+            @ValidId @PathVariable Long id,
+            @ValidQuantity @RequestParam int quantity
+    ){
+        adminProductService.updateQuantity(id, quantity);
+        return ResponseEntity.ok(ApiResponse.ok("Quantity updated successfully"));
+    }
+
+//    for adding tags to specific product
+//    like offer tag, discount tag
+//    checkbox bata tag select garney and list banayera pathaune (checking if the product already has that tag frintend mai )
+    @PutMapping("/add-tag-to-product/{productId}")
+    public ResponseEntity<ApiResponse<?>> addTagToProduct(
+            @RequestBody List<String> tagSlugs,
+            @ValidId
+            @PathVariable Long productId
+    ){
+        adminProductService.addTagToProduct(tagSlugs, productId);
+        return ResponseEntity.ok(
+                ApiResponse.ok("Tag added to product.")
+        );
+    }
+
+//    for removing tag from specific product
+//    like offer tag, discount tag
+//    checkbox bata tag select garney and list banayera pathaune (checking if the product already has that tag frintend mai )
+    @PutMapping("/remove-tag-from-product/{productId}")
+    public ResponseEntity<ApiResponse<?>> removeTagFromProduct(
+            @RequestBody List<String> tagSlugs,
+            @ValidId
+            @PathVariable Long productId
+    ){
+        adminProductService.removeTagFromProduct(tagSlugs, productId);
+        return ResponseEntity.ok(
+                ApiResponse.ok("Tag removed from product.")
+        );
+    }
+
+//    update short description
+    @PutMapping("/update-short-description/{productId}")
+    public ResponseEntity<ApiResponse<?>> updateShortDescription(
+            @RequestBody String shortDescription,
+            @ValidId
+            @PathVariable Long productId
+    ){
+        adminProductService.updateShortDescription(productId, shortDescription);
+        return ResponseEntity.ok(
+                ApiResponse.ok("Short description updated successfully.")
+        );
+    }
+
+//    update long description
+    @PutMapping("/update-long-description/{productId}")
+    public ResponseEntity<ApiResponse<?>> updateLongDescription(
+            @RequestBody String longDescription,
+            @ValidId
+            @PathVariable Long productId
+    ){
+        adminProductService.updateLongDescription(productId, longDescription);
+        return ResponseEntity.ok(
+                ApiResponse.ok("Long description updated successfully.")
+        );
+    }
+
+
+}
+
---
+++ src/main/java/com/ecommerce/service/product/AdminProductService.java
@@ -0,0 +1,239 @@
+package com.ecommerce.service.product;
+
+import com.ecommerce.dto.request.product.*;
+import com.ecommerce.dto.response.product.SingleProductResponse;
+import com.ecommerce.dto.response.product.SingleProductWithCostPriceResponse;
+import com.ecommerce.exception.ApplicationException;
+import com.ecommerce.mapper.product.BrandMapper;
+import com.ecommerce.mapper.product.CategoryMapper;
+import com.ecommerce.mapper.product.ProductImageMapper;
+import com.ecommerce.mapper.product.TagMapper;
+import com.ecommerce.model.product.*;
+import com.ecommerce.repository.product.*;
+import com.ecommerce.utils.HelperClass;
+import com.ecommerce.utils.ImageUploadHelper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+public class AdminProductService {
+
+    private final BrandRepository brandRepository;
+    private final CategoryRepository categoryRepository;
+    private final TagRepository tagRepository;
+    private final ProductRepository productRepository;
+    private final ProductImageRepository productImageRepository;
+
+    private final BrandMapper brandMapper;
+    private final TagMapper tagMapper;
+    private final CategoryMapper categoryMapper;
+    private final ProductImageMapper productImageMapper;
+
+
+    @Transactional
+    public void addBrand(BrandRequest brandRequest, MultipartFile logo) {
+        BrandModel brandModel = new BrandModel();
+        brandModel.setName(brandRequest.name().trim());
+        brandModel.setSlug(HelperClass.generateSlug(brandRequest.name().trim()));
+        String url = ImageUploadHelper.uploadImage(logo, brandRequest.name().trim());
+        brandModel.setLogoUrl(url);
+
+        brandRepository.save(brandModel);
+    }
+
+    @Transactional
+    public void addCategory(CategoryRequest categoryRequest, MultipartFile image) {
+        CategoryModel categoryParent = categoryRepository.findBySlug(categoryRequest.parentSlug().trim())
+                .orElse(null);
+        CategoryModel categoryModel = new CategoryModel();
+        categoryModel.setName(categoryRequest.name());
+        categoryModel.setSlug(HelperClass.generateSlug(categoryRequest.name().trim()));
+        categoryModel.setParent(categoryParent);
+        String url = ImageUploadHelper.uploadImage(image, categoryRequest.name().trim());
+        categoryModel.setImageUrl(url);
+        categoryRepository.save(categoryModel);
+    }
+
+    @Transactional
+    public void addTags(TagRequest request){
+        List<String> incomingNames = request.names().stream()
+                .map(String::trim)
+                .toList();
+//        generating slugs of all tags
+        List<String> incomingSlugs = incomingNames.stream()
+                .map(HelperClass::generateSlug)
+                .toList();
+
+        Set<String> existingSlugs = tagRepository.findSlugsBySlugIn(incomingSlugs);
+
+        List<TagModel> newTagModels = new ArrayList<>();
+
+        for (String name : incomingNames) {
+            String slug = HelperClass.generateSlug(name);
+
+            if (!existingSlugs.contains(slug)) {
+                newTagModels.add(new TagModel(name, slug));
+            }
+        }
+
+        if (!newTagModels.isEmpty()) {
+            tagRepository.saveAll(newTagModels);
+        }
+    }
+
+    @Transactional
+    public SingleProductWithCostPriceResponse addNewProduct(ProductRequest request, List<MultipartFile> imageFiles){
+
+//        handling brands
+        BrandModel brand = brandRepository.findBySlug(request.brandSlug().trim())
+                .orElseThrow(() -> new ApplicationException("Brand not found", "BRAND_NOT_FOUND", HttpStatus.NOT_FOUND));
+//        handling category
+        CategoryModel category = categoryRepository.findBySlug(request.categorySlug().trim())
+                .orElseThrow(() -> new ApplicationException("Category not found!", "CATEGORY_NOT_FOUND", HttpStatus.NOT_FOUND));
+//        handling tags
+        Set<TagModel> tags = new HashSet<>();
+        for (String slug : request.tagSlugs()) {
+            String cleanSlug = slug.trim().toLowerCase();
+            if (cleanSlug.isBlank()) continue;
+            TagModel tag = tagRepository.findBySlug(cleanSlug)
+                    .orElseThrow(() -> new ApplicationException("Tag not found!", "TAG_NOT_FOUND", HttpStatus.NOT_FOUND));
+            tags.add(tag);
+        }
+//        Creating Product
+        ProductModel product = new ProductModel();
+        product.setSku(request.sku().trim().toUpperCase());
+        product.setTitle(request.title().trim());
+        product.setSlug(HelperClass.generateSlug(request.title().trim()));
+        product.setShortDescription(request.shortDescription().trim());
+        product.setDescription(request.description().trim());
+        product.setCostPrice(request.costPrice());
+        product.setSellingPrice(request.sellingPrice());
+        product.setStock(request.stock() != null ? request.stock() : 0);
+        product.setSizeMl(request.sizeMl());
+        product.setActive(request.active());
+
+        product.addTags(tags);
+        brand.addProduct(product);
+        category.addProduct(product);
+
+
+        List<ProductImageModel> imageModels = new ArrayList<>();
+
+        if (imageFiles != null && !imageFiles.isEmpty()) {
+            for (int i = 0; i < imageFiles.size(); i++) {
+                MultipartFile file = imageFiles.get(i);
+                AddProductImageRequest imgReq = request.images().get(i);
+                ProductImageModel imageModel = ImageUploadHelper.uploadImage(file, imgReq);
+                imageModels.add(imageModel);
+            }
+        }
+        imageModels.forEach(product::addImage);
+        // 6. Save everything (cascade handles images + tags)
+        ProductModel savedProduct = productRepository.save(product);
+
+        return getAdminDetailOfProduct(savedProduct.getId());
+    }
+
+
+//    --
+    public SingleProductWithCostPriceResponse getAdminDetailOfProduct(Long id) {
+        ProductModel product = productRepository.findProductDetailsById(id)
+                .orElseThrow(()->new ApplicationException("Product not found", "PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND));
+
+        SingleProductResponse productResponse = new SingleProductResponse(
+                product.getId(),
+                product.getSku(),
+                product.getTitle(),
+                product.getShortDescription(),
+                product.getDescription(),
+                brandMapper.mapEntityToBrandResponse(product.getBrand()),
+                categoryMapper.mapEntityToCategoryResponse(product.getCategory()),
+                product.getSellingPrice(),
+                product.getStock(),
+                product.getSizeMl(),
+                product.getTags().stream().map(tagMapper::mapEntityToTagResponse).toList(),
+                product.getImages().stream().map(productImageMapper::mapEntityToProductImageResponse).toList()
+        );
+
+        return new SingleProductWithCostPriceResponse(productResponse, product.getCostPrice());
+    }
+
+    @Transactional
+    public void updatePrice(Long productId, BigDecimal price) {
+        ProductModel productModel = productRepository.findById(productId)
+                .orElseThrow(()->
+                        new ApplicationException("Product not found!", "PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND));
+        productModel.setSellingPrice(price);
+        productRepository.save(productModel);
+    }
+
+    @Transactional
+    public void updateQuantity(Long productId, int quantity) {
+        ProductModel productModel = productRepository.findById(productId)
+                .orElseThrow(()->
+                        new ApplicationException("Product not found!", "PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND));
+
+        Integer newStock = productModel.getStock()+quantity;
+        productModel.setStock(newStock);
+        productRepository.save(productModel);
+    }
+
+    @Transactional
+    public void addTagToProduct(List<String> slugs, Long productId){
+        ProductModel productModel = productRepository.findProductByIdWithTags(productId)
+                .orElseThrow(()->
+                        new ApplicationException("Product not found!", "PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND));
+
+        Set<TagModel> tags = new HashSet<>();
+        slugs.forEach(slug->{
+            TagModel tagModel = tagRepository.findBySlug(slug)
+                    .orElseThrow(()->new ApplicationException("Tag not found!", "TAG_NOT_FOUND", HttpStatus.NOT_FOUND));
+            if(!productModel.getTags().contains(tagModel)){
+                tags.add(tagModel);
+            }
+        });
+        productModel.addTags(tags);
+    }
+
+    @Transactional
+    public void removeTagFromProduct(List<String> slugs, Long productId){
+        ProductModel productModel = productRepository.findProductByIdWithTags(productId)
+                .orElseThrow(()->
+                        new ApplicationException("Product not found!", "PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND));
+
+        Set<TagModel> tags = new HashSet<>();
+        slugs.forEach(slug->{
+            TagModel tagModel = tagRepository.findBySlug(slug)
+                    .orElseThrow(()->new ApplicationException("Tag not found!", "TAG_NOT_FOUND", HttpStatus.NOT_FOUND));
+            if(productModel.getTags().contains(tagModel)){
+                tags.add(tagModel);
+            }
+        });
+        tags.forEach(productModel::removeTag);
+    }
+
+    public void updateShortDescription(Long productId, String shortDescription) {
+        ProductModel product = productRepository.findById(productId)
+                .orElseThrow(()->new ApplicationException("Product not found!", "PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND));
+        product.setShortDescription(shortDescription);
+        productRepository.save(product);
+    }
+
+    public void updateLongDescription(Long productId, String longDescription) {
+        ProductModel product = productRepository.findById(productId)
+                .orElseThrow(()->new ApplicationException("Product not found!", "PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND));
+        product.setDescription(longDescription);
+        productRepository.save(product);
+    }
+
+}
